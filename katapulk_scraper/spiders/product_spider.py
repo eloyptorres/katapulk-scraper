d41173440025af36713f524aa301d942da2125b3
Eloy Pérez Torres <eloyptorres@gmail.com>
2021-09-15 01:30:05 -0400

    Add product spider to extract items' information

---
+++ katapulk_scraper/spiders/product_spider.py
@@ -0,0 +1,25 @@
+import scrapy
+import json
+
+class ProductSpider(scrapy.Spider):
+    name = 'products'
+    start_urls = ['https://www.katapulk.com/search?&zone_id=6']
+
+    def parse(self, response):
+        products = response.css('div.src-components-styles-components-___product-tile__textBlock___286s4')
+        recognized_products = dict()
+        for product in products:
+            title = product.css('h4.src-components-styles-components-___product-tile__titleCard___3kCr3 a::text').get()
+            description = product.css('div.src-components-styles-components-___product-tile__productDescription___3Imvy p::text').get()
+            price = product.css('div.src-components-styles-components-___product-tile__price___249i-::text').get()
+            link = product.css('h4.src-components-styles-components-___product-tile__titleCard___3kCr3 a::attr(href)').get()
+            recognized_products[len(recognized_products)] = dict(title=title, description=description, price=price, link=link)
+
+        filename = 'products.json'
+        with open(filename, 'w') as f:
+            json.dump(recognized_products, f, indent=4)
+        
+        filename = 'katapulk.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved filename {filename}')
