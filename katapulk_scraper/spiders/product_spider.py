99c3c4dba05325352eccd5270db46ae4a29de482
Eloy Pérez Torres <eloyptorres@gmail.com>
2021-09-16 00:33:02 -0400

    Improve readability of url detection process

--- katapulk_scraper/spiders/product_spider.py
+++ katapulk_scraper/spiders/product_spider.py
@@ -12,14 +12,11 @@ class ProductSpider(scrapy.Spider):
             title = product.css('h4.src-components-styles-components-___product-tile__titleCard___3kCr3 a::text').get()
             description = product.css('div.src-components-styles-components-___product-tile__productDescription___3Imvy p::text').get()
             price = product.css('div.src-components-styles-components-___product-tile__price___249i-::text').get()
-            link = product.css('h4.src-components-styles-components-___product-tile__titleCard___3kCr3 a::attr(href)').get()
-            recognized_products[len(recognized_products)] = dict(title=title, description=description, price=price, link=link)
+            relative_url = product.css('h4.src-components-styles-components-___product-tile__titleCard___3kCr3 a::attr(href)').get()
+            url = None if relative_url is None else response.urljoin(relative_url)
+            recognized_products[len(recognized_products)] = dict(title=title, description=description, price=price, url=url)
 
         filename = 'products.json'
         with open(filename, 'w') as f:
             json.dump(recognized_products, f, indent=4)
-        
-        filename = 'katapulk.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved filename {filename}')
+        self.log(f'Saved {len(recognized_products)} products to {filename}')
