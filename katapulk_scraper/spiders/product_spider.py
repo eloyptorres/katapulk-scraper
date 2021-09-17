702a8fc6e6c07f28c4fcd1bb9319c5f5674ba0d4
Eloy Pérez Torres <eloyptorres@gmail.com>
2021-09-17 14:19:24 -0400

    Add space's cleanse to avoid 'different' products

--- katapulk_scraper/spiders/product_spider.py
+++ katapulk_scraper/spiders/product_spider.py
@@ -5,13 +5,18 @@ class ProductSpider(scrapy.Spider):
     name = 'products'
     start_urls = ['https://www.katapulk.com/']
 
+    @staticmethod
+    def clean_spaces(string: str):
+        ''' Remove leading, trailing and multiple whitespaces in string'''
+        return ' '.join(string.split())
+
     def parse(self, response):
         products = response.css('div.src-components-styles-components-___product-tile__textBlock___286s4')
         recognized_products = dict()
         for product in products:
-            title = product.css('h4.src-components-styles-components-___product-tile__titleCard___3kCr3 a::text').get()
-            description = product.css('div.src-components-styles-components-___product-tile__productDescription___3Imvy p::text').get()
-            price = product.css('div.src-components-styles-components-___product-tile__price___249i-::text').get()
+            title = ProductSpider.clean_spaces(product.css('h4.src-components-styles-components-___product-tile__titleCard___3kCr3 a::text').get())
+            description = ProductSpider.clean_spaces(product.css('div.src-components-styles-components-___product-tile__productDescription___3Imvy p::text').get())
+            price = ProductSpider.clean_spaces(product.css('div.src-components-styles-components-___product-tile__price___249i-::text').get())
             relative_url = product.css('h4.src-components-styles-components-___product-tile__titleCard___3kCr3 a::attr(href)').get()
             url = None if relative_url is None else response.urljoin(relative_url)
             recognized_products[title+price] = dict(title=title, description=description, price=price, url=url)
